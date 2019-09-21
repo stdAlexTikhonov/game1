e7b8e553c30e0660d6746b87695d5c83b83fd683
Alexander <stdAlexTikhonov@hotmail.com>
2019-09-21 23:35:48 +0300

    Some fixes

--- src/js/script.js
+++ src/js/script.js
@@ -1,33 +1,32 @@
 let time = 0;
 let frames = 0;
-let flag = false;
-
 
 const main = () => {
     if (frames % FPS === 0) {
         store.dispatch({type: SET_TIMER});
         const { game, player } = store.getState();
         const { history, process } = game;
-      
-        if (flag && player.timer === 0) {
-            store.dispatch({type: SET_DIRECTION});
-            switch(player.direction) {
-                case LEFT:
-                    store.dispatch({type: SET_PLAYER_X, position: player.x - 1});
-                    break;
-                case RIGHT:
-                    store.dispatch({type: SET_PLAYER_X, position: player.x + 1});
-                    break;
-                case UP:
-                    store.dispatch({type: SET_PLAYER_Y, position: player.y - 1});
-                    break;
-                case DOWN:
-                    store.dispatch({type: SET_PLAYER_Y, position: player.y + 1});
-                    break;
-            }
-        }
 
         if (process) {
+
+            if (player.timer === 0) {
+                store.dispatch({type: SET_DIRECTION});
+                switch(player.direction) {
+                    case LEFT:
+                        store.dispatch({type: SET_PLAYER_X, position: player.x - 1});
+                        break;
+                    case RIGHT:
+                        store.dispatch({type: SET_PLAYER_X, position: player.x + 1});
+                        break;
+                    case UP:
+                        store.dispatch({type: SET_PLAYER_Y, position: player.y - 1});
+                        break;
+                    case DOWN:
+                        store.dispatch({type: SET_PLAYER_Y, position: player.y + 1});
+                        break;
+                }
+            }
+            
             time++;
             //drawing==========================
             clearWindow();
@@ -35,11 +34,11 @@ const main = () => {
             //logging==========================
             // console.log("STEP:",time, "GAME IN PROGRESS")
             // console.log("DIRECTION:", player.direction);
-            console.log("POS timer: ", player.timer);
+            // console.log("POS timer: ", player.timer);
         } else {
             console.log("GAME PAUSED")
         }
-        flag = true;
+
     }
     window.requestAnimationFrame(main);
     frames++;
