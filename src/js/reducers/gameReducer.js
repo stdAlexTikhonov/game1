8e302077108a903638bc42350fc2662d2b460aa4
Alexander <stdAlexTikhonov@hotmail.com>
2019-10-09 10:18:56 +0300

    TASK-12 Timeline + game reducers

--- src/js/reducers/gameReducer.js
+++ src/js/reducers/gameReducer.js
@@ -1,7 +1,9 @@
 const initGame = {
     pause: false,
     process: false,
-    timer: 0
+    timer: 0,
+    direction: null,
+    index: 0
 };
 
 const gameReducer = (state = initGame, action) => {
@@ -26,6 +28,32 @@ const gameReducer = (state = initGame, action) => {
             return {
                 ...state,
                 pause: true
+            }
+        case SWIPE_TIME_LEFT:
+            return {
+                ...state,
+                direction: LEFT
+            }
+        case SWIPE_TIME_RIGHT:
+            return {
+                ...state,
+                direction: RIGHT
+            }
+        case PAUSE_TIME:
+            return {
+                ...state,
+                direction: null
+            }
+        case SET_INDEX:
+            return {
+                ...state,
+                index: state.index + action.index
+            }
+        case RESET_TIMELINE:
+            return {
+                ...state,
+                direction: null,
+                index: 0
             }
         default:
             return state;
