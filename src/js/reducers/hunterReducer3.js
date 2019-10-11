d0c812104ce1901a9292601c43a75facf2515a89
Alexander <stdAlexTikhonov@hotmail.com>
2019-10-12 00:20:50 +0300

    Refctor

--- src/js/reducers/hunterReducer3.js
+++ src/js/reducers/hunterReducer3.js
@@ -1,31 +1,31 @@
-const initHunter2 = {
-    x: 10,
-    y: 7,
+const initHunter = {
+    x: 9,
+    y: 8,
     path: [],
     history: [],
     currentStep: null,
     passedCells: []
 };
 
-const hunterReducer2 = (state = initHunter2, action) => {
+const hunterReducer3 = (state = initHunter, action) => {
     switch(action.type) {
-        case SET_PATH2:
+        case SET_PATH3:
             return {
                 ...state,
                 path: action.path
             }
-        case SET_HUNTER_DIRECTION2: 
+        case SET_HUNTER_DIRECTION3: 
             return {
                 ...state,
                 currentStep: state.path.shift() 
             }
-        case SET_HUNTER_POSITION_FROM_HISTORY2: 
+        case SET_HUNTER_POSITION_FROM_HISTORY3: 
             return {
                 ...state,
                 x: action.x,
                 y: action.y
             }
-        case SET_HUNTER_POSITION2: {
+        case SET_HUNTER_POSITION3: {
             let {x, y, currentStep} = state;
             const { x: xnew, y: ynew } = currentStep ? setHunterPosition(x,y,currentStep) : {x, y};
             
@@ -36,7 +36,7 @@ const hunterReducer2 = (state = initHunter2, action) => {
                 passedCells: state.passedCells.includes(`${ynew + '' + xnew}`) ? state.passedCells : state.passedCells.concat([`${ynew + '' + xnew}`]),
             }
         }
-        case SAVE_HUNTER2: {
+        case SAVE_HUNTER3: {
             const { history } = state;
             history.unshift({x: action.x, y: action.y});
             return {
