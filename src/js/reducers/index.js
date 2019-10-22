4d201e5e4f3b7c057f704874ff827edc4234a646
Alexander <stdAlexTikhonov@hotmail.com>
2019-10-22 09:17:04 +0300

    Init react

--- src/js/reducers/index.js
+++
@@ -1,9 +0,0 @@
-const app = (state = {}, action) => {
-    return {
-        player: playerReducer(state.player, action),
-        game: gameReducer(state.game, action),
-        hunter: hunterReducer(state.hunter,action),
-        hunter2: hunterReducer2(state.hunter2, action),
-        hunter3: hunterReducer3(state.hunter3, action)
-    }
-}
