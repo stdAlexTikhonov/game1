f6509c1e30de50fa43fc9e297cf93067932e7443
Alexander <stdAlexTikhonov@hotmail.com>
2019-10-24 22:12:15 +0300

    test

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,17 +1,48 @@
 import React, { Component } from 'react'
+import { FPS, CELL_WIDTH, WALL_COLOR, WINDOW_HEIGHT, WINDOW_WIDTH, BACKGROUND, MAP_ } from '../utils/constants'
 
 const height = window.innerHeight,
 width = document.body.clientWidth
 
 class Canvas extends Component {
     componentDidMount() {
-        this.iter = 0;
+        this.frame = 0;
         this.start();
+    }
+
+    clearWindow = () => {
+        const ctx = this.refs.canvas.getContext('2d');
+        ctx.clearRect(0,0,WINDOW_WIDTH,WINDOW_HEIGHT); // clear canvas
+        ctx.fillStyle = BACKGROUND;
+        ctx.fillRect(0,0,WINDOW_WIDTH,WINDOW_HEIGHT);
+    }
+
+    drawMap = () => {
+        const ctx = this.refs.canvas.getContext('2d');
+        // const { player } = store.getState();
+    
+        MAP_.forEach((item,i) => {
+          item.forEach((elem,j) => {
+            if (elem === 0) { 
+                context.fillStyle = WALL_COLOR;
+                context.fillRect(CELL_WIDTH*j, CELL_WIDTH * i, CELL_WIDTH, CELL_WIDTH);
+            } 
+            // else {
+            //     if (!player.foodMap.includes(`${i + '' + j}`)) {
+            //         context.beginPath();
+            //         context.arc(CELL_WIDTH*j + CELL_WIDTH/2, CELL_WIDTH * i + CELL_WIDTH/2, FOOD_SIZE, 0, 2 * Math.PI, false);
+            //         context.fillStyle = FOOD_COLOR;
+            //         context.fill();
+            //         context.closePath();
+            //     }
+            // }
+          });
+        });
     }
 
     updateCanvas() {
         const ctx = this.refs.canvas.getContext('2d');
-        ctx.fillRect(0,0, width, height);
+        ctx.fillRect(0,0, WINDOW_WIDTH, WINDOW_HEIGHT);
     }
 
     componentWillUnmount(){
@@ -22,19 +53,24 @@ class Canvas extends Component {
         if (!this.frameId) {
           this.frameId = requestAnimationFrame(this.animate)
         }
-        this.updateCanvas();
+        
     }
 
     stop = () => {
         const ctx = this.refs.canvas.getContext('2d');
         cancelAnimationFrame(this.frameId)
-        ctx.clearRect(0,0, width, height);
+        ctx.clearRect(0,0, WINDOW_WIDTH, WINDOW_HEIGHT);
     }
 
     animate = () => {
-       this.frameId = window.requestAnimationFrame(this.animate)
-       this.iter++;
-       if (this.iter > 100) this.stop();
+        this.frameId = window.requestAnimationFrame(this.animate)
+        this.frame++;
+
+        if (frames % FPS === 0) {
+            this.clearWindow();
+            this.drawMap();
+        }
+        if (this.frame > 100) this.stop();
     }
 
     render() {
